3bc03ceb7fcc8dde9cce4956d4d86e49d82361f4
mufeng <mufeng.me@gmail.com>
2026-08-28 16:40:06 +0800

    feat(redis): support JSON TLS options

--- packages/server/src/config/bull/index.ts
+++ packages/server/src/config/bull/index.ts
@@ -5,22 +5,14 @@ import {
   BULL_JOB_BACKOFF_DELAY,
   BULL_JOB_BACKOFF_TYPE,
   BULL_JOB_TIMEOUT,
-  REDIS_DB,
-  REDIS_HOST,
-  REDIS_PASSWORD,
-  REDIS_PORT,
-  REDIS_USERNAME
+  REDIS_DB
 } from '@environments'
 import { ms } from '@heyform-inc/utils'
 
+import { createRedisOptions } from '../redis'
+
 export const BullOptionsFactory = (): BullModuleOptions | Promise<BullModuleOptions> => ({
-  redis: {
-    host: REDIS_HOST,
-    port: REDIS_PORT,
-    username: REDIS_USERNAME,
-    password: REDIS_PASSWORD,
-    db: REDIS_DB + 1
-  },
+  redis: createRedisOptions(REDIS_DB + 1),
   defaultJobOptions: {
     attempts: BULL_JOB_ATTEMPTS,
     timeout: ms(BULL_JOB_TIMEOUT),
--- packages/server/src/config/redis/index.ts
+++ packages/server/src/config/redis/index.ts
@@ -2,19 +2,35 @@ import {
   RedisModuleOptions,
   RedisModuleOptionsFactory
 } from '@svtslv/nestjs-ioredis/dist/redis.interfaces'
+import { RedisOptions } from 'ioredis'
 
-import { REDIS_DB, REDIS_HOST, REDIS_PASSWORD, REDIS_PORT, REDIS_USERNAME } from '@environments'
+import {
+  REDIS_DB,
+  REDIS_HOST,
+  REDIS_PASSWORD,
+  REDIS_PORT,
+  REDIS_TLS,
+  REDIS_USERNAME
+} from '@environments'
+import { toJSON } from '@heyform-inc/utils'
+
+export function createRedisOptions(db: number = REDIS_DB, tls: string = REDIS_TLS): RedisOptions {
+  const tlsOptions = toJSON<NonNullable<RedisOptions['tls']>>(tls)
+
+  return {
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    username: REDIS_USERNAME,
+    password: REDIS_PASSWORD,
+    db,
+    ...(tlsOptions ? { tls: tlsOptions } : {})
+  }
+}
 
 export class RedisService implements RedisModuleOptionsFactory {
   createRedisModuleOptions(): Promise<RedisModuleOptions> | RedisModuleOptions {
     return {
-      config: {
-        host: REDIS_HOST,
-        port: REDIS_PORT,
-        username: REDIS_USERNAME,
-        password: REDIS_PASSWORD,
-        db: REDIS_DB
-      }
+      config: createRedisOptions()
     }
   }
 }
